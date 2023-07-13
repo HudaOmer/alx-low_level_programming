2a757e41e35dca45d1374ace592db057a7ea081a
HudaOmer <hudaoe175@gmail.com>
2023-07-13 16:16:03 -0700

    commit_message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include <studio.h>
+
+/**
+ * This is a comment
+ * another
+ */
+
+int main(void){
+
+	printf("Programming is like building a multilingual puzzle\n");
+	return(0);
+}
