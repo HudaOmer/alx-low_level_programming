44931f4c2ff7852ed6b8541340bbb4047eec3d06
HudaOmer <hudaoe175@gmail.com>
2023-07-13 16:39:34 -0700

    commit_message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 /**
- * This is a comment
- * another
+ * main - entry point
+ * 
+ * Description: prints a sentance using puts
+ *
+ * Return: Always 0 (success)
  */
 
-int main(void){
-
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
