f64b09f41d0562aab85b1407af580cdc095341ca
HudaOmer <hudaoe175@gmail.com>
2023-07-13 16:29:06 -0700

    commit_message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * This is a comment
- * another
+ * main - entry point
+ * 
+ * Description: prints a sentance using puts
+ *
+ * Return: Always 0 (success)
  */
-
-int main(void){
-
-	puts("\"Programming is like building a multilingual puzzle\n");
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
