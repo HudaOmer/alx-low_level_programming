3a2811d1cdc8a942fe9a2e023d0a401e15469873
HudaOmer <hudaoe175@gmail.com>
2023-07-14 13:10:25 -0700

    commit_message

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: prints alphabets
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar((char)i);
+	for (i = 97; i <= 103; i++)
+		putchar((char)i)
+	putchar('\n');
+
+	return (0);
+}
