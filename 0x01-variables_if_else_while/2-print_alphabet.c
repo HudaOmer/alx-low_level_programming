947b7b04cb384d9449d09be994cab13960cff01c
HudaOmer <hudaoe175@gmail.com>
2023-07-14 11:21:22 -0700

    commit_message

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: prints alphabets
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char ch = 'a'
+
+	for (ch = 'a';ch <= 'z'; ch++)
+		putchar(ch);
+	putchar(' ');
+}
