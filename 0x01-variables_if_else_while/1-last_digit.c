5dd5aadc9efcbca1add257a32c6593338f92946a
HudaOmer <hudaoe175@gmail.com>
2023-07-14 11:05:33 -0700

    commit_message

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,11 @@
 /**
  * main - main function
  *
- * Description:  more headers goes there */
+ * Description:  more headers goes there 
+ *
+ * Return: Always 0 (success)
+ *
+ */
 
 int main(void)
 {
@@ -15,15 +19,17 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	mod = n % 10;
-	printf("Last digit of %i is %i",n, mod);
+	printf("Last digit of %i is %i", n, mod);
 	if (mod > 5)
 	{
-	printf(" and is greater than 5");
+	printf(" and is greater than 5\n");
 	} else if (n < 6 && n != 0)
 	{
-	printf(" and is less than 6 and not 0");
+	printf(" and is less than 6 and not 0\n");
 	} else
-	printf(" and is 0");
+	{
+	printf(" and is 0\n");
+	}
 	/* end of my code */
 	return (0);
 }
