02aae7de3538d614326ca3fb571297de73a91fe3
HudaOmer <hudaoe175@gmail.com>
2023-07-14 13:02:33 -0700

    commit_message

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: prints alphabets
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch <= 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
