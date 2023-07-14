2d63325724dc8632ad171d647bae8dcb938870f4
HudaOmer <hudaoe175@gmail.com>
2023-07-14 15:45:37 -0700

    commit_message

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: prints alphabets
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i == 8 && j == 9)
+				;
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
