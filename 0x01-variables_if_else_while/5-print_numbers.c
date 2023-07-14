3784cc4887cc4cf60c9ab8105a10bf694ef5b284
HudaOmer <hudaoe175@gmail.com>
2023-07-14 12:38:02 -0700

    commit_message

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: prints numbers
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf(i);
+	putchar('\n');
+
+	return (0);
+}
