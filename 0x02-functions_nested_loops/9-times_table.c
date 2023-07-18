1a47da8719280f22e3f1294afbc2a0f0d56bf946
HudaOmer <hudaoe175@gmail.com>
2023-07-18 16:45:19 -0700

    solve problems

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for(j = 0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
 			num = i * j;
 			if (j != 0)
@@ -28,12 +28,16 @@ void times_table(void)
 			if (num < 10)
 			{
 			_putchar(num + '0');
-			}else
+			}
+			else
 			{
 			_putchar(num / 10 + '0');
 			_putchar(num % 10 + '0');
 			}
-			_putchar(',');
+			if (j != 9)
+			{
+				_putchar(',');
+			}
 		}
 		_putchar('\n');
 	}
