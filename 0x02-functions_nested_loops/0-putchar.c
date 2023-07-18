a6048b2cafb6ef59d11962addb4857199c91b517
HudaOmer <hudaoe175@gmail.com>
2023-07-18 13:47:34 -0700

    solve problems

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,6 +11,14 @@
 
 int main(void)
 {
-	my_print("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
