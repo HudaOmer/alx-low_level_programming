f676e7ef29a60a2c99dd2ba080b7640d26c024f8
HudaOmer <hudaoe175@gmail.com>
2023-07-18 18:45:57 -0700

    solve problems

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Description: summ of %5 %3 =0
+ * Return: 0 Always (success)
+ */
+
+int main(void)
+{
+	int i, sum = 0;
+	for (i = 3; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
