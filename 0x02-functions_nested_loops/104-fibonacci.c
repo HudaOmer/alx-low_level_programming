b8940acc4c0239e993a6448265e50093d4591af4
HudaOmer <hudaoe175@gmail.com>
2023-07-18 18:39:12 -0700

    solve problems

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,17 +13,17 @@ int main(void)
 
 	arr[0] = 1;
 	arr[1] = 2;
-	printf("%lu, " ,arr[0]);
-	printf("%lu" ,arr[1]);
+	printf("%lu, ", arr[0]);
+	printf("%lu", arr[1]);
 	for (i = 2; i < 92; i++)
-	{	
-		arr[i] = arr[i-1] + arr[i-2];
+	{
+		arr[i] = arr[i - 1] + arr[i - 2];
 		if (i != 91)
-			printf(", %lu" ,arr[i]);
+			printf(", %lu", arr[i]);
 	}
 	i--;
-	j1 = arr[i-1] / 1000000000;
-	j2 = arr[i-1] % 1000000000;
+	j1 = arr[i - 1] / 1000000000;
+	j2 = arr[i - 1] % 1000000000;
 	k1 = arr[i] / 1000000000;
 	k2 = arr[i] % 1000000000;
 	for (i = 92; i < 99; ++i)
