6ede9c2546ec85dc2d96d100644147508cc7cc53
HudaOmer <hudaoe175@gmail.com>
2023-07-18 17:43:21 -0700

    solve problems

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * print_times_table - func
+ * @n: inpun int
+ * Description: If n is greater than 15 or less than 0 the function should not print anything
+ *
+ * Return: void
+ */
+
+void print_times_table(int n)
+{
+	int i, j, num;
+	if (n > 15 || n < 0)
+		return ;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			num = i * j;
+			if (j != 0)
+			{
+				_putchar(' ');
+				if (num < 100)
+				{
+					_putchar(' ');
+					if (num < 10)
+					{
+						_putchar(' ');
+					}
+				}
+			}
+			if (num < 10)
+			{
+			_putchar(num + '0');
+			}
+			else if (num < 100)
+			{
+			_putchar(num / 10 + '0');
+			_putchar(num % 10 + '0');
+			}
+			else
+			{
+			_putchar(num / 100 + '0');
+			_putchar((num / 10) % 10 + '0');
+			_putchar(num % 10 + '0');
+			}
+			if (j != n)
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+
+}
