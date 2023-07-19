17fc1384593859d69b70f89f7bd2ac43489fa835
HudaOmer <hudaoe175@gmail.com>
2023-07-18 18:53:44 -0700

    solve problems

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main 
+ * Description: fibonacci
+ * Return: 0 Always (success)
+ */
+
+int main(void)
+{
+	long int *arr;
+	int i;
+
+	arr[0] = 1;
+	arr[1] = 2;
+	printf("%li, ", arr[0]);
+	printf("%li", arr[1]);
+	for (i = 2; i < 50; i++)
+	{
+		arr[i] = arr[i - 1] + arr[i - 2];
+		printf(", %li", arr[i]);
+	}
+}
