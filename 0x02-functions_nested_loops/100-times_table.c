688be1200d0dc01d166084f74d84f747a078e5f8
HudaOmer <hudaoe175@gmail.com>
2023-07-18 17:48:31 -0700

    solve problems

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,10 +8,10 @@
 void print_times_table(int n)
 {
 	int i, j, num;
+
 	if (n > 15 || n < 0)
-		return ;
-	for (i = 0; i <= n; i++)
-	{
+		return;
+	for (i = 0; i <= n; i++) {
 		for (j = 0; j <= n; j++)
 		{
 			num = i * j;
