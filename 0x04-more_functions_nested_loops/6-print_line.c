d9746ae85945d3c10807da0ee879b37d63239ad6
HudaOmer <hudaoe175@gmail.com>
2023-07-20 13:28:02 -0700

    solve tasks of project 4

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - print
+ * @n: int input
+ * Dscription: prints line
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
+}
