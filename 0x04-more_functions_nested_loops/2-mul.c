a5a5394a1d1941260bd3bcf495e7e506be3a638a
HudaOmer <hudaoe175@gmail.com>
2023-07-20 12:58:28 -0700

    solve tasks of project 4

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiply numbers
+ * @a: int input
+ * @b:int input
+ * Description: multiply
+ *
+ * Return: the result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
