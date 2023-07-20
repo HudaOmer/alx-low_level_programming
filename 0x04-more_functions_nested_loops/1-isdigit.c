44c66f23b5226598b49f3c42cbdfeb7210a79626
HudaOmer <hudaoe175@gmail.com>
2023-07-20 11:55:12 -0700

    solve tasks of project 4

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - check the code.
+ * @ch: an input character
+ * Description: find upper or nor
+ * Return: 1 when digit 0 otherwise.
+ */
+
+int _isdigit(char ch)
+{
+	if (ch >= '0' && ch <= '9')
+		return (1);
+	else
+		return (0);
+}
