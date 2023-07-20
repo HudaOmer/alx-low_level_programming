3381f6fe7a67e7c2cc52a02f5d36292a2a3f6baf
HudaOmer <hudaoe175@gmail.com>
2023-07-20 13:22:17 -0700

    solve tasks of project 4

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - multiply numbers
+ * Description: multiply
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i > 9)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
