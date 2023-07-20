5c85058b1233efd182beec109841d3e60eb905fd
HudaOmer <hudaoe175@gmail.com>
2023-07-20 13:47:45 -0700

    solve tasks of project 4

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print
+ * @n: int input
+ * Dscription: prints line
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
