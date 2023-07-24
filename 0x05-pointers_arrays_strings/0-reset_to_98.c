5d609bb55cd4a8dc4bfcbe7cbdb4ff35ec3228fc
HudaOmer <hudaoe175@gmail.com>
2023-07-24 14:29:28 -0700

    solve pointers and arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - change value by pointer
+ * @*n: a pointer input
+ * Description: does the job
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
