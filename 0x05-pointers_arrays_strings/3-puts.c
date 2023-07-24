84a0bfe11c0a96183e84534ab0b7f2e96171e1b2
HudaOmer <hudaoe175@gmail.com>
2023-07-24 15:44:22 -0700

    solve pointers and arrays

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -11,6 +11,7 @@
 void _puts(char *str)
 {
 	int count = 0;
+
 	while (*(str + count) != '\0')
 	{
 		_putchar(*(str + count));
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,6 +11,7 @@
 void print_rev(char *s)
 {
 	int count = 0;
+
 	while (*(s + count) != '\0')
 	{
 		count++;
