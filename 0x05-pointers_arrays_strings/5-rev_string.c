d8699ff0ea5cf1eba7d0409b86c1d63e39fc75d6
HudaOmer <hudaoe175@gmail.com>
2023-07-24 16:17:32 -0700

    solve pointers and arrays

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,39 @@
 #include "main.h"
 
 /**
- * */
+ * rev_string - function
+ * @s: string input
+ * Description: reverse the string
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i, count = 0;
+	char temp;
+
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+	count--;
+	if (count % 2 != 0)
+	{
+		for (i = count; i >= (count + 1) / 2; i--)
+		{
+			temp = *(s + i);
+			*(s + i) = *(s + count - i);
+			*(s + count - i) = temp;
+		}
+	}
+	else
+	{
+		for (i = count; i >= count / 2; i--)
+		{
+			temp = *(s + i);
+			*(s + i) = *(s + count - i);
+			*(s + count - i) = temp;
+		}
+	}
+}
