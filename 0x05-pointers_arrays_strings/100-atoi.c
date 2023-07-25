ecbada77f2b6539ed9e720a455d3fb340ae74f75
HudaOmer <hudaoe175@gmail.com>
2023-07-24 19:56:50 -0700

    solve pointers and arrays

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,31 +11,29 @@
 
 int _atoi(char *s)
 {
-	int i = 0, j, count = 0, minus = 0;
-	unsigned int num = 0 , ten = 1;
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-	while (!(*(s + count) >= '0' && *(s + count) <= '9'))
-		{
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+
 		if (*(s + count) == '-')
-			minus++;
-		count++;
-	}
- 	j = count;
-	while (*(s + count) >= '0' && *(s + count) <= '9')
-	{
-		ten *= 10;
-		count++;
-		i++;
-	}
-	count = j;
-	for (; i > 0; i--)
-	{
-		ten /= 10;
-		num += (*(s + count) - 48) * ten;
-		count++;
-	}
-	if (minus % 2 == 1)
-		num *= -1;
-	return (num);
+			pn *= -1;
 
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
+
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
 }
