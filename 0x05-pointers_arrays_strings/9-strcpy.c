4a7d4f2099f4eafcd2df2f7f09962b0cd3d288e3
HudaOmer <hudaoe175@gmail.com>
2023-07-24 17:17:17 -0700

    solve pointers and arrays

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,7 +13,8 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i, count = 0;
-	while(src[count] != '\0')
+
+	while (src[count] != '\0')
 		count++;
 	for (i = 0; i < count; i++)
 		dest[i] = src[i];
