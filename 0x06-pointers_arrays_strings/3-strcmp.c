e1bad9ac08a4a4f75368ffaf9bd5408485e7dcc6
HudaOmer <hudaoe175@gmail.com>
2023-07-26 16:29:16 -0700

    solve project 6

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,11 +16,10 @@ int _strcmp(char *s1, char *s2)
 	while (areequal == 0)
 		{
 			if ((s1[count] == '\0') && (s2[count] == '\0'))
-			{
 				break;
-			}
 			areequal = s1[count] - s2[count];
 			count++;
 		}
+
 	return (areequal);
 }
