a657fa2edf1e91806f1bae907ee748c6b30a31df
HudaOmer <hudaoe175@gmail.com>
2023-07-26 17:04:39 -0700

    solve project 6

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,26 @@
 #include "main.h"
 
-/***/
+/**
+ * reverse_array - reverse
+ * @a: pointer to int a array
+ * @b: poimter to int b size of array
+ * Description: reverses the content of an array of integers
+ * 
+ * Return: void
+ */
 
 void reverse_array(int *a, int n)
+{
+	unsigned int temp;
+	int i = 0;
+
+	n--;
+	while (i <= n / 2)
+	{
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
+		i++;
+		n--;
+	}
+}
