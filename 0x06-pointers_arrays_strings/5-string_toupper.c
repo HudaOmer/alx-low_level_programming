730e194a97e4e678670b70b48b95e678a9f58606
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-07-27 19:23:57 +0200

    Create 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
+
+char *string_toupper(char *s)
+{
+  int count = 0;
+  while (n[count] != '\0')
+    {
+      if (s[count] >= 'a' && s[count] <= 'z')
+        n[count] = n[count] - 32;
+      count++;
+    }
+  return (n);
+}
