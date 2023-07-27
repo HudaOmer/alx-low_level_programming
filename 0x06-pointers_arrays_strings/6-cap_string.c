83e3fa3ce72585c6d345743d1f5d8cb6252ac5ea
HudaOmer <hudaoe175@gmail.com>
2023-07-27 11:59:31 -0700

    solve project 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,21 +1,25 @@
 #include "main.h"
 
-\**
+/**
  * cap_string - capitalize
  * @s: pointer to array
  *
  * Return: pointer to the string
- *\
+ */
 
 char *cap_string(char *s)
 {
-	int count = 0;
+	int i, count = 0;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-  	while (s[count] != '\0')
- 	 {
-   		 if (s[count] == ' ' && s[count + 1] >= 'a' && s[count + 1] <= 'z')
-    	 		 s[count + 1] = s[count + 1] - 32;
-   		 count++;        
-	  }
-	  return (n);
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] = s[0] - 32;
+	while (s[count] != '\0')
+	{
+		for (i = 0; i < 13; i++)
+			if ((s[count] == sep_words[i]) && s[count + 1] >= 'a' && s[count + 1] <= 'z')
+				s[count + 1] = s[count + 1] - 32;
+		count++;
+	}
+	return (s);
 }
