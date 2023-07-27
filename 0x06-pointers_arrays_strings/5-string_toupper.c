1f6bf8696d2239b5b6d72890ab7c6d3b3ece5432
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-07-27 19:25:03 +0200

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,10 +10,10 @@ char *string_toupper(char *s)
 {
     int count = 0;
     while (n[count] != '\0')
-      {
+    {
         if (s[count] >= 'a' && s[count] <= 'z')
-          n[count] = n[count] - 32;
+            n[count] = n[count] - 32;
         count++;
-      }
+    }
     return (n);
 }
