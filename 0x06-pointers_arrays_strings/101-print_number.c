2c8d51e6ce361b09afa5d19079070d23b7136c8e
HudaOmer <hudaoe175@gmail.com>
2023-07-27 15:50:04 -0700

    solve project 6

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
+ */
+
+void print_number(int n)
+{
+	unsigned int i, ten = 1, count = 0, x;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	if (n == 0)
+		_putchar('0');
+	x = n;
+	while (x > 0)
+	{
+		x /= 10;
+		count++;
+	}
+	for (i = 1; i < count; i++)
+		ten *= 10;
+	while (count)
+	{
+		_putchar((n / ten) % 10 + '0');
+		count--;
+		ten /= 10;
+	}
+}
