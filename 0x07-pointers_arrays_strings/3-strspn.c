91356f362509d8186741199abcafe94dd5e6bdef
HudaOmer <hudaoe175@gmail.com>
2023-07-31 16:10:29 -0700

    solve pointers pj7

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,7 +4,8 @@
  * _strspn - function
  * @s: array to look in
  * @accept: array to search for
- * Return: no of occurances*/
+ * Return: no of occurances
+ */
 
 unsigned int _strspn(char *s, char *accept)
 {
