95a39e0a52c5d39df5efc1a3e9950c6d76006a56
HudaOmer <hudaoe175@gmail.com>
2023-07-31 15:33:44 -0700

    solve pointers pj7

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - a function
+ * @s: pointer to array
+ * @b: character to insert
+ * @n: first n bytes of the memory area pointed to by s
+ * Return: pointer to s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
