d0dee531b16e2881b39df16937889adb01899200
HudaOmer <hudaoe175@gmail.com>
2023-07-31 15:53:15 -0700

    solve pointers pj7

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - search for a chatacter
+ * @s: poiter to string
+ * @c: character to be found
+ * Return: pointer to where c is found
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return NULL;
+}
