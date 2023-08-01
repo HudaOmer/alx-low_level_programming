9432d40a063763e6e8211ab8826007bbf0a9616a
HudaOmer <hudaoe175@gmail.com>
2023-07-31 17:38:50 -0700

    solve pointers pj7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - print type
+ * @a: array 2d int
+ * @size: square size
+ * Return: soid
+ */
+
+void print_diagsums(int *a, int size)
+{
+	unsigned long sum1 = 0, sum2 = 0;
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i + i * size];
+		sum2 += a[size * (i + 1) - (i + 1)];
+	}
+	printf("%lu, %lu\n", sum1, sum2);
+}
