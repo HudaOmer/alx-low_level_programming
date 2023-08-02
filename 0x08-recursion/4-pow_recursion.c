d3760671fb3d36d3e16f960030da24a12b235ab4
HudaOmer <hudaoe175@gmail.com>
2023-08-02 14:12:34 -0700

    solve recursion pjt 8

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - power, math
+ * @x: base number
+ * @y: power
+ * Return: the power
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
+	else if (y == 0)
+		return (1);
+	else
+		return(-1);
+}
