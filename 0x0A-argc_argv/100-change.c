046f8b262ccb25e31dd79bfda360ec1c9d67a9a5
HudaOmer <hudaoe175@gmail.com>
2023-08-04 15:08:27 -0700

    solve project 10 Args

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,30 +13,30 @@ int main(int argc, char **argv)
 	int cents, i = 0, count = 0;
 	int coins[5] = {25, 10, 5, 2 ,1};
 
-	if (argc == 2)
-	{
-		cents = atoi(argv[1]);
-
-		if (cents <= 0)
-		{
-			printf("%d\n", 0);
-			return (1);
-		}
-		while (cents > 0 && i < 5)
-		{
-			if (cents >= coins[i])
-			{
-				count += cents / coins[i];
-				cents = cents % coins[i];
-			}
-			i++;
-		}
-	}
-	else
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	cents = atoi(argv[1]);
+
+	if (cents <= 0)
+	{
+		printf("%d\n", 0);
+		return (1);
+	}
+
+	while (cents > 0 && i < 5)
+	{
+		if (cents >= coins[i])
+		{
+			count += cents / coins[i];
+			cents = cents % coins[i];
+		}
+		i++;
+	}
+
 	printf("%d\n", count);
 	return (0);
 }
