f53d25fccac028a7a6cd575701a36519599d26d0
HudaOmer <hudaoe175@gmail.com>
2023-08-04 14:38:28 -0700

    solve project 10 Args

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - main function
+ * @argc: int (arg count)
+ * @argv: array (arg vector)
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i, not = 0, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		while (argv[i][not] != '\0')
+		{
+			if (!isdigit(argv[i][not]))
+			{
+				printf("Error\n, %d\n", sum);
+				return (1);
+			}
+			not++;
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
---
+++ 0x0A-argc_argv/ertyhj.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * my_atoi - is there a char?
+ * @s: string
+ * return: 0 or 1
+ */
+
+int my_atoi(char *s)
+{
+	int not = 0;
+
+	while (*s != '\0')
+	{
+		if (*s >= '0' && *s <= '9')
+			printf("%c", *s);
+		else
+		{
+			not = 1;
+			printf("not%c\t", *s);
+		}
+		s++;
+	}
+	return (not);
+}
+
+/**
+ * main - main function
+ * @argc: int (arg count)
+ * @argv: array (arg vector)
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i, not = 0, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (my_atoi((char *)argv + i) == 1)
+		{
+			not = 1;
+			break;
+		}
+		else
+			sum += atoi(argv[i]);
+	}
+	if (not)
+		printf("Error\n, %d", sum);
+	else
+		printf("%d\n", sum);
+	return (not);
+}
