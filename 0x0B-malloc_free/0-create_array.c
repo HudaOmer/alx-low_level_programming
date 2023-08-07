fed7766e84009d6d970e52794328d8d6125019ad
HudaOmer <hudaoe175@gmail.com>
2023-08-07 11:44:30 -0700

    solve project 11 malloc and free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * create_array - create an array
+ * @size: array size
+ * @c: charater, initialize the array with it
+ * Return: NULL or the allocated array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+
+	if (!size)
+		return (NULL);
+	s = malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
+	{
+		*(s + i) = c;
+	}
+	return (s);
+}
