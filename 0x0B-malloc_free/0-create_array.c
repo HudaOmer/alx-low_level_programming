d1cad13b8a9f4f98dd3bccacb37a015c6bb25708
HudaOmer <hudaoe175@gmail.com>
2023-08-07 11:48:36 -0700

    solve project 11 malloc and free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,6 +17,8 @@ char *create_array(unsigned int size, char c)
 	if (!size)
 		return (NULL);
 	s = malloc(sizeof(char) * size);
+	if (s == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		*(s + i) = c;
