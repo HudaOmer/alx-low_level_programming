96f0206f2b3ecaf62a763a3d1c64f127d2648aeb
HudaOmer <hudaoe175@gmail.com>
2023-08-08 07:37:32 -0700

    solve project 11 malloc and free

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,60 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _size - determine the size os an array
+ * @s: the array
+ * Return: int (size)
+ */
+
+int _size(char *s)
+{
+	int i = 0, count = 0;
+
+	while(*(s + i) != '\0')
+	{
+		count++;
+		i++;
+	}
+	return (count);
+}
+
+/**
+ * str_concat - concatinate two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to the allocated new string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int i = 0, isnull = 0, size = 0;
+
+	size = _size(s1) + _size(s2) + 1;
+	s = malloc(sizeof(char) * size);
+	if (s1 != NULL)
+		while (*s1 != '\0')
+		{
+			*(s + i) = *s1;
+			s1++;
+			i++;
+		}
+	else
+		isnull++;
+
+	if (s2 != NULL)
+		while (*s2 != '\0')
+		{
+			*(s + i)= *s2;
+			s2++;
+			i++;
+		}
+	else
+		isnull++;
+	*(s + i) = '\0';
+	if (isnull == 2)
+		return (NULL);
+	return	(s);
+}
