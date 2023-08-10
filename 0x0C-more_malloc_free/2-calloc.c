b6a176e1c3f77dee642e6b516f9ec206fded5bcd
HudaOmer <hudaoe175@gmail.com>
2023-08-10 16:10:22 -0700

    solve allocation pjt 12

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - function
+ * @nmemb:
+ * @size: the size
+ * Return:  pointer to the allocated memory.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *s;
+
+	if (size == 0)
+		return (NULL);
+	s = malloc(nmemb * size);
+	if (s == NULL)
+		return (NULL);
+	return (s);
+}
