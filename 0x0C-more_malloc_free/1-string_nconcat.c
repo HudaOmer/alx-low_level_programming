6b2afa2f8fa34b89008d68e55b850c00897939b0
HudaOmer <hudaoe175@gmail.com>
2023-08-10 17:30:32 -0700

    solve allocation pjt 12

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,43 +12,31 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int size1 = 0, size2 = 0, size = 0, i;
 	char *s;
-	unsigned int i = 0, size = 0;
 
-	while (*(s2 + i) != '\0')
-	{
-		size++;
-		i++;
-	}
-	if (size < n)
-		n = size;
-	i = 0;
-	while (*(s1 + i) != '\0')
-	{
-		size++;
-		i++;
-	}
-	s = malloc(sizeof(char) * (size + n + 1));
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (size1 = 0; s1[size1] != '\0'; size1++)
+		;
+	for (size2 = 0; s2[size2] != '\0'; size2++)
+		;
 
-	i = 0;
+	if (size2 < n)
+		n = size2;
+	size = size1 + n;
+	s = malloc(sizeof(char) * (size + 1));
 	if (s == NULL)
 		return (NULL);
-	if (s1 != NULL)
-		while (*s1 != '\0' && size != 0)
-		{
-			*(s + i) = *s1;
-			s1++;
-			size--;
-			i++;
-		}
-	if (s2 != NULL)
-		while (*s2 != '\0' && n != 0)
-		{
-			*(s + i) = *s2;
-			s2++;
-			i++;
-			n--;
-		}
-	*(s + i) = '\0';
-	return	(s);
+	for (i = 0; i < size; i++)
+	{
+		if (i < size1)
+			s[i] = s1[i];
+		else
+			s[i] = s2[i - size1];
+	}
+	s[i] = '\0';
+	return (s);
 }
