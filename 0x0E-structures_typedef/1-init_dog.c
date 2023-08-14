1a08f8703a4797af36a1c9852d566b81fe8d3019
HudaOmer <hudaoe175@gmail.com>
2023-08-14 13:18:38 -0700

    solve 0x0E-structures_typedef pjt 14

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - initilize struct function
@@ -11,6 +12,9 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		d = malloc(sizeof (struct dog));
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
