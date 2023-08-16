54f74f181fbb27f8c7e4350cf209c442d06833f4
HudaOmer <hudaoe175@gmail.com>
2023-08-16 11:34:38 -0700

    solve pointers to functions pjt

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - print array
+ * @size: of array 
+ * @action: pointer to function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (!array || !action)
+		return ;
+	while (size)
+	{
+		size--;
+		action(*array);
+		array++;
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef FUNCTIONS_MAIN
 #define FUNCTIONS_MAIN
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
