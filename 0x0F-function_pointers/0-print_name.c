cca9da10976332be9c16ce5fe838f2a68bca0d19
HudaOmer <hudaoe175@gmail.com>
2023-08-16 09:59:13 -0700

    solve recursion pjt 8

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - pass function pointer
+ * @name: string
+ * @f: pointer to function
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+Solve 0x0F-function_pointers project
