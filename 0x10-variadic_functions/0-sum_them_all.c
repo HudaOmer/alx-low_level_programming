3f95b50b2f174d6c45cc2fe7490341a4dd187785
HudaOmer <hudaoe175@gmail.com>
2023-08-17 13:33:37 -0700

    solve 0x10-variadic_functions pjt 16

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum
+ * @n: numbers to be summed
+ * Return: the sum or 0 if n == 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int oh, sum = 0;
+	va_list variadic; 
+
+	oh = n;
+	if (oh == 0)
+	{
+		return (0);
+	}
+	va_start(variadic, n);
+	while (oh)
+	{
+		sum += va_arg(variadic, int);
+		oh--;
+	}
+	return (sum);
+}
