09998a8138a3cac8b7b95d1e898e399681c89140
HudaOmer <hudaoe175@gmail.com>
2023-08-17 13:38:03 -0700

    solve 0x10-variadic_functions pjt 16

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef VARIACIC_FUNCTION
 #define VARIADIC_FUNCTION
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
