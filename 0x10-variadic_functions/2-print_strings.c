32cefb3482e9406a11afda28174078ba2a6952fb
HudaOmer <hudaoe175@gmail.com>
2023-08-17 15:48:03 -0700

    solve 0x10-variadic_functions pjt 16

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (check)
 			printf("%s", check);
 		else
-			printf("(nill)");
+			printf("(nil)");
 		if (separator && count < n - 1)
 			printf("%s", separator);
 	}
