78488e0eb4ec3797b839c8aced650ba1bf8f67ba
HudaOmer <hudaoe175@gmail.com>
2023-08-17 17:16:09 -0700

    solve 0x10-variadic_functions pjt 16

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,6 +3,7 @@
 /**
  * sum_them_all - sum the ints
  * @n: the required int
+ *
  * Return: the summ
  */
 
@@ -15,7 +16,9 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(mylist, n);
 
 	for (count = 0; count < n; count++)
+	{
 		sum += va_arg(mylist, int);
+	}
 
 	va_end(mylist);
 	return (sum);
