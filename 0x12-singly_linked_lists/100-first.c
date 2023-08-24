989570b20dd931eac595c1c76e8aa3798cf44db0
HudaOmer <hudaoe175@gmail.com>
2023-08-24 16:26:34 -0700

    OMG main of main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * myStartupFun - executes before main
+ */
+
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
