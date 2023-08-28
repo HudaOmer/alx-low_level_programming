c60066b626eca16cc399d44f4aa3535ac760fa49
HudaOmer <hudaoe175@gmail.com>
2023-08-28 14:04:49 -0700

    Solve task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free all allocated pointers
+ * and set head to null
+ * @head: pointer to the head of linked list
+ * Return: nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *loop;
+
+	while (*head)
+	{
+		loop = (*head)->next;
+		free(*head);
+		*head = loop;
+	}
+
+}
