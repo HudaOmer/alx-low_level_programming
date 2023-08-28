3262e630839181438604d7bcd9504fc6d9baec52
HudaOmer <hudaoe175@gmail.com>
2023-08-28 12:09:20 -0700

    Solve task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - counts the number of nodes of a listint_t list
+ * @h: listint_t type
+ * Return: the number of nodes
+ */
+	
+size_t listint_len(const listint_t *h)
+{
+	size_t n = 0;
+	const listint_t *loop;
+
+	loop = h;
+	while (loop)
+	{
+		loop = loop->next;
+		n++;
+	}
+	return (n);
+}
