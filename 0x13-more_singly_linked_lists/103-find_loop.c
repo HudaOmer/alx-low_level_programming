b50aa71ed9420a91dc05038606842e79e305a423
HudaOmer <hudaoe175@gmail.com>
2023-08-29 19:06:19 -0700

    Solve task A.103

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -14,22 +14,22 @@ listint_t *find_listint_loop(listint_t *head)
 
 	if (!head)
 		return (NULL);
-	while (p1 && p2 && p2->next)
+
+	while (slow && fast && fast->next)
 	{
 		p1 = p1->next;
 		p2 = p2->next->next;
 		if (p1 == p2)
 		{
-			break;
+			p1 = head;
+			while (p1->next != p2->next)
+			{
+				p1 = p1->next;
+				p2 = p2->next;
+			}
+			return (p1->next);
 		}
 	}
-	if (!p2 || !p2->next)
-		return (NULL);
-	p1 = head;
-	while (p1->next != p2->next)
-	{
-		p1 = p1->next;
-		p2 = p2->next;
-	}
-	return (p1->next);
+
+	return (NULL);
 }
