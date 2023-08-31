3025c43766ec28bf394dd7a9408cfba03fb7e82c
HudaOmer <hudaoe175@gmail.com>
2023-08-31 16:49:54 -0700

    Solve task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_binary - print binary representation of a number
+ * @n: number to print
+ * Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n)
+	{
+		if (n >> 1)
+			print_binary(n >> 1)
+		_putchar((n & 1) + '0');
+	}
+	else
+		_putchar('0');
+}
