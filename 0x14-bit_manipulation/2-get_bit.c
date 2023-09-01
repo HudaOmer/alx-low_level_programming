4018eed22afef8b9f9601454782ffa7bf6360fbd
HudaOmer <hudaoe175@gmail.com>
2023-08-31 17:03:45 -0700

    Solve task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_bit - get bit at index
+ * @n: the number
+ * @index: the index of bit
+ * Return: the bit 0,1 or -1 otherwise
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > sizeof(unsigned long int)*8)
+		return (-1);
+	return ((n >> index) & 1);
+}
