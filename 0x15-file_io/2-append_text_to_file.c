e9883d90531b5d3dace70453f52ed9bc910efc47
HudaOmer <hudaoe175@gmail.com>
2023-09-04 18:33:43 -0700

    Solve task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - append
+ * @filename: string
+ * @text_content: NULL terminated string to add at the end of the file
+ * Return: 1 on success and -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, length, r;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd < 0)
+		return (-1);
+
+	for (length = 0; text_content[length]; length++)
+		;
+
+	if (text_content)
+		r = write(fd, text_content, length);
+	if (r < 0)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
