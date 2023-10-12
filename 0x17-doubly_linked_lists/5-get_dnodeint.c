ea13314570177e29d3f80e034346863d1662b162
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-10-13 02:42:28 +0300

    Solve task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -4,12 +4,12 @@
  * get_dnodeint_at_index - return some node's value
  * @head: pointer to the head of the list
  * @index: index of the node to find its value
- * Return: the value of the node or NULL on failure 
+ * Return: the value of the node or NULL on failure
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 
-	for (;head && index; index--)
+	for (; head && index; index--)
 		head = head->next;
 
 	return (head);
