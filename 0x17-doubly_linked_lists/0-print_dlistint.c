23648d2300cd69ca70e86068ef40eb2237045252
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-10-13 00:29:08 +0300

    Solve task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include lists.h""
+
+/**
+ * print_dlistint - prints elements of double linked list
+ * @h: pointer to head of the list
+ * Return: the number of nodes in the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+
+	if (!h)
+		return (count);
+
+/** make sure that you are in the head (all)*/
+	while (h->prev)
+		h = h->prev;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
