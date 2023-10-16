baf5c5b01ea255ab56c799117cfa4a9e2db12f08
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-10-16 23:04:06 +0300

    Try

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -shared -o myprintf.so -fPIC test.c
-export LD_PRELOAD=./myprintf.so:$LD_LIBRARY_PATH
+wget -P /tmp/ https://github.com/HudaOmer/alx-low_level_programming/blob/master/0x18-dynamic_libraries/makewin.so
+export LD_PRELOAD=/tmp/makewin.so
