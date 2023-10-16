73df3d81b6998e893ecd05d0d00cc0396c6c0026
HudaOmer <hudaoe175@gmail.com>
2023-10-16 05:41:50 -0700

    Solve task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include "operations.h"
+
+/**
+ * add - addition
+ * @a: addend
+ * @b: addend
+ * Return: Sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtraction
+ * @a: minuend
+ * @b: subtrahend
+ * Return: Differance
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multipication
+ * @a: multiplicand
+ * @b: multiplier
+ * Return: Product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - division
+ * @a: num
+ * @b: divider
+ * Return: Product
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulus
+ * @a: num
+ * @b: divider
+ * Return: mod
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
