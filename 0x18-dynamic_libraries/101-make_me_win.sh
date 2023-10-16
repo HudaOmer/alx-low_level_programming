63e4d996e6501188a6ed6b9fcf5739b7bd83540e
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-10-16 23:18:58 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/HudaOmer/alx-low_level_programming/blob/7bf05dc5550b7ed14d02f6bef56b69843433f168/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://raw.github.com/HudaOmer/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
