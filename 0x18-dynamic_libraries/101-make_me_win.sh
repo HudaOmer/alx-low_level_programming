7317233cc7fd0fe0e8ed140b64c4301db3164713
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-10-16 20:01:22 +0300

    Solve Advanced 101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp/ https://github.com/HudaOmer/alx-low_level_programming/tree/master/0x18-dynamic_libraries/random.so
+wget -p /tmp/ https://github.com/sidneyriffic/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
 export LD_PRELOAD=/tmp/random.so
