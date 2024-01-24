ad353dcecc6849c2bf1366ac62f8d7965bb1daa9
HudaOmer <hudaoe175@gmail.com>
2024-01-24 10:52:27 -0800

    Solve task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This  module defines island_perimeter function"""
+
+
+def island_perimeter(grid):
+    """A function that counts the perimeter of an island"""
+    rows = len(grid)
+    columns = len(grid[0])
+    result = 0
+    for i in range(0, rows):
+        for j in range(0, columns):
+            if (grid[i][j] == 1):
+                if (i == 0):
+                    result += 1
+                elif i == rows - 1:
+                    result += 1
+                else:
+                    if grid[i+1][j] == 0:
+                        result += 1
+                    if grid[i-1][j] == 0:
+                        result += 1
+                if j == 0:
+                    result += 1
+                elif j == columns - 1:
+                    result += 1
+                else:
+                    if grid[i][j+1] == 0:
+                        result += 1
+                    if grid[i][j-1] == 0:
+                        result += 1
+    return result
